3982217110572d5452192bc66388d3501aacff9c
Zachary Piesner <zachapiesner@gmail.com>
2026-06-12 14:58:35 -0400

    Add: valid-palindrome-ii - submission-1

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-1.py
@@ -0,0 +1,33 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        pal_ready = ''
+        for char in s:
+            if char.isalnum():
+                pal_ready += char.lower()
+        for x in range(len(pal_ready)):
+            skip = False
+            temp_pal = pal_ready[:x] + pal_ready[x+1:]
+            a = 0
+            b = len(temp_pal) - 1
+            if x == len(pal_ready):
+                b += 1
+                while a < b:
+                    if pal_ready[a] != pal_ready[b]:
+                        skip = True
+                        break
+                    a += 1
+                    b -= 1
+            else:
+                while a < b:
+                    if temp_pal[a] != temp_pal[b]:
+                        skip = True
+                        break
+                    a += 1
+                    b -= 1
+            if skip == True:
+                continue
+            return True
+        return False
+
+        sean_is_asscheeks = True
+            
